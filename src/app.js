d3f7ddac20cbcd0f53bc31267f39fc4f77b9483f
ozali <ali.ozkardesler@gmail.com>
2021-10-26 21:37:59 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log("Merhaba Kodlama.io");
+
+
+let dolarDun = 9.5;
+
+
+console.log(dolarBugun);
+let dolarBugun = 9.30;
+
+console.log(dolarDun);
+
+const euroDun = 11.2;
+//euroDun= 10;
+
+console.log(euroDun);
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"];
+console.log(konutKredileri);
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>")
+};
+    
